a2ebb86bd8ba8c1156ac1125859e19fd6262fe66
vijay <vijay2340025@gmail.com>
2024-03-02 23:27:55 +0530

    initial commit

---
+++ src/main/java/org/sample/BasicProducer.java
@@ -0,0 +1,14 @@
+package org.sample;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BasicProducer {
+    private static Logger log = LoggerFactory.getLogger(BasicProducer.class.getName());
+
+    public static void main(String[] args) {
+        log.info("Hello World!!!");
+
+        
+    }
+}
